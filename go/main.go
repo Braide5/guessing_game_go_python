147b9654c550189f8269a991f14699c7b329d2c0
Simon Samuel <salviensky@gmail.com>
2023-02-17 22:36:45 +0100

    BREAKING CHANGE: User input causes infinite loop

--- go/main.go
+++ go/main.go
@@ -1,20 +1,29 @@
 package main
-import "fmt"
-import "math/rand"
+
+import (
+	"fmt"
+	"math/rand"
+)
+
 func main() {
 	fmt.Println("welcome to the amazing game!")
-	random_number = randint(0, 20)
+	random_number := rand.Intn(100)
 
-	for{
-	guess = int(input("enter a number between 0 and 20"))
+	for {
+		fmt.Println("Enter a number between 0 and 100")
+		var guess int
+		if _, err := fmt.Scanln("%d", &guess); err != nil {
+			fmt.Println("Please enter a valid number")
+			continue
+		}
 
-	if guess < random_number
-		fmt.println("you guessed too low")
-	elif guess > random_number:
-		fmt.println("you guessed too high")
-
-	else:
-		fmt.println("you guessed correctly")
-
-	exist}
+		if guess < random_number {
+			fmt.Println("you guessed too low")
+		} else if guess > random_number {
+			fmt.Println("you guessed too high")
+		} else {
+			fmt.Println("you guessed correctly")
+		}
+		break
+	}
 }
--- go/text_sample.go
+++ go/text_sample.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
+func text_sample() {
 	fmt.Println("hey")
 }
