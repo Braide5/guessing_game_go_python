07843850639a03c9ff4a60de13c1bea6e4e1825b
Simon Samuel <salviensky@gmail.com>
2023-02-18 22:20:42 +0100

    fix: resolved all logical errors

--- go/main.go
+++ go/main.go
@@ -3,27 +3,38 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
 	fmt.Println("welcome to the amazing game!")
-	random_number := rand.Intn(100)
+
+	// Declaring the random_number and assigning it a random number
+	source := rand.NewSource(time.Now().UnixNano())
+	random_generator := rand.New(source)
+
+	random_number := random_generator.Intn(100)
+	fmt.Println("Enter a number between 0 and 100")
 
 	for {
-		fmt.Println("Enter a number between 0 and 100")
+
+		// Declaring the guess variable to hold an integer
 		var guess int
-		if _, err := fmt.Scanln("%d", &guess); err != nil {
+
+		_, err := fmt.Scanln(&guess)
+
+		if err != nil {
 			fmt.Println("Please enter a valid number")
 			continue
 		}
 
 		if guess < random_number {
-			fmt.Println("you guessed too low")
+			fmt.Println("you guessed too low, please try again")
 		} else if guess > random_number {
-			fmt.Println("you guessed too high")
+			fmt.Println("you guessed too high, please try again")
 		} else {
 			fmt.Println("you guessed correctly")
+			break
 		}
-		break
 	}
 }
