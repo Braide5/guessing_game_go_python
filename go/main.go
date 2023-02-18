3c08d42740f5d7df226bbdaa70744b50db9c1e82
Simon Samuel <salviensky@gmail.com>
2023-02-18 22:31:51 +0100

    docs: added comments

--- go/main.go
+++ go/main.go
@@ -9,10 +9,13 @@ import (
 func main() {
 	fmt.Println("welcome to the amazing game!")
 
-	// Declaring the random_number and assigning it a random number
+	// Creating a random number source using the current time
 	source := rand.NewSource(time.Now().UnixNano())
+
+	// Creating a random number generator using the random number source
 	random_generator := rand.New(source)
 
+	// Getting a random number between 0 and 100
 	random_number := random_generator.Intn(100)
 	fmt.Println("Enter a number between 0 and 100")
 
@@ -21,13 +24,18 @@ func main() {
 		// Declaring the guess variable to hold an integer
 		var guess int
 
+		// Reading from the console and storing the value in the guess variable
 		_, err := fmt.Scanln(&guess)
 
+		// If the user enters a non-integer value, we print an error message
+		// and continue
 		if err != nil {
 			fmt.Println("Please enter a valid number")
 			continue
 		}
 
+		// If the user enters a number less than 0 or greater than 100,
+		// we print an error message and continue
 		if guess < random_number {
 			fmt.Println("you guessed too low, please try again")
 		} else if guess > random_number {
